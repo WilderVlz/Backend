49829a6d532d99d9843ed2cea15edc1fac8a1542
Wilder Ruiz Velez <124533466+WilderVlz@users.noreply.github.com>
2023-07-24 17:23:13 -0500

    Delete Descargas/practiceCRUD/PracticeCRUD directory

--- Descargas/practiceCRUD/PracticeCRUD/src/main/java/com/example/PracticeCRUD/Repository/RepoPerson.java
+++
@@ -1,7 +0,0 @@
-package com.example.PracticeCRUD.Repository;
-
-import com.example.PracticeCRUD.Entity.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepoPerson extends CrudRepository<Person,Integer> {
-}
